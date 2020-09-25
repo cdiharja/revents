1a3337b25b664dbcbe685dcd5b420c630fe664f3
Chandra <chandra.diharja@powerfront.com>
2020-09-25 14:37:00 +1000

    add template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+import EventDashboard from './features/events/eventDashboard/EventDashboard';
+import NavBar from './features/nav/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+    
+     <NavBar/>
+     <Container className="main">
+        <EventDashboard/>
+     </Container>
+    </Fragment>
   );
 }
 
---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Container, Menu,Button} from 'semantic-ui-react';
+
+const NavBar = ()=>{
+    return (
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    <img src="../logo192.png" alt="logo"/>
+                    Re-vent
+                </Menu.Item>
+                <Menu name="events"/>
+                <Menu.Item>
+                    <Button positive inverted content="Create Event"></Button>
+                </Menu.Item>
+                <Menu.Item position="right">
+                    <Button positive inverted content="Login"></Button>
+                    <Button positive inverted content="Register" style={{marginLeft:'5px'}}></Button>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+};
+
+export default NavBar;
